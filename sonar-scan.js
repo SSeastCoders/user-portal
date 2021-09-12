7488e29dae071afa6aa74e37153da1fb11a6cc41
Luis Fernandez <luis.fernandez@smoothstack.com>
2021-09-12 18:56:02 -0400

    reports ts

--- sonar-scan.js
+++ sonar-scan.js
@@ -9,7 +9,7 @@ sonarqubeScanner(
       //"sonar.tests": "./src/__tests__", // set up leter with dev team
       "sonar.test.inclusions": "./src/__tests__/**/*.test.tsx,./src/__tests__/**/*.test.ts",
       "sonar.typescript.lcov.reportPaths": "coverage/lcov.info",
-      "sonar.testExecutionReportPaths": "./reports/test-report.xml",
+      "sonar.testExecutionReportPaths": "reports/test-report.xml",
     },
   },
   () => {},
