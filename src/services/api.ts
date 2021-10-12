01e01f278b9ba91beafb1912f610618a491fb74c
Luis Fernandez <luis.fernandez@smoothstack.com>
2021-10-12 00:18:26 -0400

    connect user-api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-// export const BASE_URL = `${process.env.REACT_APP_USER_ENDPOINT}`;
-export const BASE_URL = "http://a67c630576f2a4662bf0686fb807857e-718136710.us-east-1.elb.amazonaws.com:8222";
+export const BASE_URL = `${process.env.REACT_APP_USER_ENDPOINT}`;
+//export const BASE_URL = "http://a67c630576f2a4662bf0686fb807857e-718136710.us-east-1.elb.amazonaws.com:8222";
 export const ACCOUNT_ENDPOINT = `${process.env.REACT_APP_ACCOUNT_ENDPOINT}`;
 export const TRANSACTION_ENDPOINT = `${process.env.REACT_APP_TRANSACTION_ENDPOINT}`;
